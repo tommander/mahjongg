76465721bf048f732fa7ae80a003ecae81707241
tommander <tomas.rajnoha@proton.me>
2025-06-13 01:06:47 +0200

    HTML file for development that can have SRI added via script

--- sri.sh
+++ sri.sh
@@ -1,8 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "script.js:"
-echo -n "sha256-" && sha256sum -b script.js | grep -o '^[0-9a-f]\+' | xxd -r -p | base64
-echo "language.js:"
-echo -n "sha256-" && sha256sum -b language.js | grep -o '^[0-9a-f]\+' | xxd -r -p | base64
-echo "style.css:"
-echo -n "sha256-" && sha256sum -b style.css | grep -o '^[0-9a-f]\+' | xxd -r -p | base64
+SCRIPTJS=`echo -n "sha256-" && sha256sum -b script.js | grep -o '^[0-9a-f]\+' | xxd -r -p | base64`
+LANGJS=`echo -n "sha256-" && sha256sum -b language.js | grep -o '^[0-9a-f]\+' | xxd -r -p | base64`
+STYLECSS=`echo -n "sha256-" && sha256sum -b style.css | grep -o '^[0-9a-f]\+' | xxd -r -p | base64`
+
+cat dev.html | sed "s|src=\"script.js\"|src=\"script.js\" integrity=\"$SCRIPTJS\"| ; s|src=\"language.js\"|src=\"language.js\" integrity=\"$LANGJS\"| ; s|href=\"style.css\"|href=\"style.css\" integrity=\"$STYLECSS\"|" - > index.html
