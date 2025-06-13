b9cac08d73a1c3bf9db96048ec76ebc96940e450
Tomáš Rajnoha <tomas.rajnoha@proton.me>
2025-06-14 01:56:34 +0200

    Cleanup after finishing #8 (#16)

--- release.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "Creating TAR.XZ..."
-tar -cJvf releases/release.tar.xz background.webp index.html language.js LICENSE script.js style.css
-echo "Creating ZIP..."
-zip -j releases/release.zip background.webp index.html language.js LICENSE script.js style.css
--- sri.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SCRIPTJS=`echo -n "sha256-" && sha256sum -b script.js | grep -o '^[0-9a-f]\+' | xxd -r -p | base64`
-LANGJS=`echo -n "sha256-" && sha256sum -b language.js | grep -o '^[0-9a-f]\+' | xxd -r -p | base64`
-STYLECSS=`echo -n "sha256-" && sha256sum -b style.css | grep -o '^[0-9a-f]\+' | xxd -r -p | base64`
-
-cat dev.html | sed "s|src=\"script.js\"|src=\"script.js\" integrity=\"$SCRIPTJS\"| ; s|src=\"language.js\"|src=\"language.js\" integrity=\"$LANGJS\"| ; s|href=\"style.css\"|href=\"style.css\" integrity=\"$STYLECSS\"|" - > index.html
